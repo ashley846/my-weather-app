a34d13c4459be79810b463821944494141e5e0ea
ashley846 <buhlempho02@gmail.com>
2024-11-19 12:15:55 +0200

    added some js

---
+++ scr/weather.js
@@ -0,0 +1,55 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humilityElement = document.querySelector("#humility");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.date.time * 1000);
+  let icon = '<img src="${esponse" class="weather-app-icon" />';
+
+  cityElement.innerHTML = formatDate(data);
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humilityElement.innerHTML = "${response.data.temperature.humility}%";
+  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
+  timeElement.innerHTML =
+    "${data.getDate()}:${data.getHour()}:${date.getMinutes()}";
+}
+
+function formatDate(data) {
+  let hours = data.getHour();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[data.getDate()];
+  if (minutes < 10) {
+    minutes = "0${minutes}";
+  }
+  return "${day} ${hours}:${minutes}";
+}
+
+function searchCity(city) {
+  let apiKEY = "0aed8e05tadf246fo437cfb406113d46";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric";
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.peventDefualt();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventlistener("submit", handleSearchSubmit);
+searchCity("Pretoria");
